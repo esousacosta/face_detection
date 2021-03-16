47e428d9ae6ef1adb2a72e29fad7f0525496a74f
Bacamartes <victoremanoel23@gmail.com>
2021-03-15 22:13:12 -0300

    Added the cascade classifier and are about to use it

--- face_detection/src/face-detection.cpp
+++ face_detection/src/face-detection.cpp
@@ -1,6 +1,11 @@
+#include <opencv2/objdetect.hpp>
 #include <opencv2/opencv.hpp>
 
 int main(int argc, char *argv[])
 {
+  // Loading the cascade classifier trained  on the data from the .xml file
+  cv::CascadeClassifier face_cascade = cv::CascadeClassifier("../haarcascade_frontalface_default.xml");
+  // Reading the input image
+  
   return 0;
 }
