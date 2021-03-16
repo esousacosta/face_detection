ef44cb2b971e78ade3582394bcbf13a3deaf7ef5
Bacamartes <victoremanoel23@gmail.com>
2021-03-15 22:18:28 -0300

    Added a window to visualize the inputimage

--- face_detection/src/face-detection.cpp
+++ face_detection/src/face-detection.cpp
@@ -1,11 +1,17 @@
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
 #include <opencv2/objdetect.hpp>
 #include <opencv2/opencv.hpp>
 
 int main(int argc, char *argv[])
 {
+  cv::Mat image;
+
+  cv::namedWindow("Face detection", cv::WINDOW_AUTOSIZE);
   // Loading the cascade classifier trained  on the data from the .xml file
   cv::CascadeClassifier face_cascade = cv::CascadeClassifier("../haarcascade_frontalface_default.xml");
   // Reading the input image
-  
+  image = cv::imread("../eu.jpg");
+  cv::imshow("Face detection", image);
   return 0;
 }
