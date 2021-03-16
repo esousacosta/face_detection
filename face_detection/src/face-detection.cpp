c2cb59d6eab78b2b2deb83cf17d6697c91a9f4fb
Bacamartes <victoremanoel23@gmail.com>
2021-03-16 08:14:47 -0300

    Changed the path to the xml file and drew the rects for the faces

--- face_detection/src/face-detection.cpp
+++ face_detection/src/face-detection.cpp
@@ -4,20 +4,27 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/objdetect.hpp>
 #include <opencv2/opencv.hpp>
+#include <stdexcept>
 
 int main(int argc, char *argv[])
 {
-  cv::Mat image, gray;
+  cv::Mat image, gray_image;
+  std::vector<cv::Rect> faces;
 
   cv::namedWindow("Face detection", cv::WINDOW_AUTOSIZE);
   // Loading the cascade classifier trained  on the data from the .xml file
-  cv::CascadeClassifier face_cascade = cv::CascadeClassifier("../haarcascade_frontalface_default.xml");
+  cv::CascadeClassifier face_cascade = cv::CascadeClassifier("/home/bacamartes/Documents/UFRN/2020_2/PDI/projeto_final/face_detection/haarcascade_frontalface_default.xml");
   // Reading the input image
   image = cv::imread("/home/bacamartes/Documents/UFRN/2020_2/PDI/projeto_final/face_detection/eu.jpg");
-  cv::cvtColor(image, gray, cv::COLOR_BGR2GRAY);
+  cv::cvtColor(image, gray_image, cv::COLOR_BGR2GRAY);
 
   if (!image.data)
 	std::cerr << "Couldn't open the requested image. Exiting!" << std::endl;
+
+  face_cascade.detectMultiScale(gray_image, faces, 1.1, 4);
+
+  for (int i = 0; i < faces.size(); i++)
+	cv::rectangle(image, cv::Rect(faces[i].x, faces[i].y, faces[i].width, faces[i].height), cv::Scalar(255, 0, 0), 2);
 
   cv::imshow("Face detection", image);
   cv::waitKey();
