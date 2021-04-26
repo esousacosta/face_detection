fa80f2c9e7671edea8933aa979397a42b0d047c7
Bacamartes <victoremanoel23@gmail.com>
2021-04-26 13:13:48 -0300

    Added dlib models to implement face swap functionnalities

--- face_detection/arduino/face_detection/face_detection.ino
+++ face_detection/arduino/face_detection/face_detection.ino
@@ -2,33 +2,96 @@
 
 int SERVO_PIN = 3;
 int led_pin = 13;
-int pot_pin = A0;
-int angle;
-int max_buffer = 3;
+unsigned int angle;
+bool receiving = false;
 Servo my_servo;
+
+// Variables to control the data input from the Serial monitor
+// the limit is four because the array is terminated by '\0'.
+const byte max_buffer = 4;
+
+// Defining the delimiting chars
+const char start_byte = '<';
+const char stop_byte = '>';
 
 /* The servo motor is controlled via angle and its limits are [0, 180]. */
 
 void setup() {
-  static char buffer[max_buffer];
   Serial.begin(9600);
   my_servo.attach(SERVO_PIN);
   my_servo.write(0);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly: 
-  /* angle = analogRead(pot_pin); */
-  /* angle = map(angle, 0, 1023, 0, 180); */
+  static byte index = 0;
+  static char buffer[max_buffer];
 
   // I want a code that checks if a received number is sandwiched in between two chars: '<' and '>',
   // and reads, at most, 3 digits for that number; if the number has more than 3 digits, I want the
-  // code to tell the user they have input an invalid number.
+  // code to tell the user they have input an invalid number. For instance, if I type: <150> in
+  // the terminal, I want the value read to be 150.
   
   if (Serial.available() > 0) {
-  angle = Serial.read() - '0';
-  my_servo.write(angle);
-  Serial.println(angle, DEC);
+	temp = Serial.read();
+	if (temp == start_byte) {
+	  index = 0;
+	  Serial.print("<");
+	  receiving = true;
+	} else if (temp == stop_byte) {
+	  buffer[index] = '\0';
+	  receiving = false;
+	  index = -1;
+	  Serial.println(">");
+	  process_input_data(buffer, angle);
+	} else {
+	  if (receiving && index < max_buffer - 1) {
+		buffer[index] = temp;
+		Serial.print(temp);
+		index++;
+	  } else if (receiving && index >= max_buffer - 1) {
+			Serial.println("Max buffer size exceeded! Discarding...");
+			receiving = false;
+			index = -1;
+			clear_serial();
+			clear_buffer(buffer);
+	  }
+	}
   }
   delay(20);
+}
+
+
+void process_input_data(char buffer[], unsigned int &angle)
+{
+  // This function doesn't work as the array is passed as a pointer to the
+  // array's first element, messing up the memory access for memcpy.
+  angle = atoi(buffer);
+  control_servo(angle);
+  clear_buffer(buffer);
+  clear_serial();
+}
+
+void control_servo(unsigned int angle)
+{
+  // This function send the desired angle to the motor and
+  // writes it on the serial montior.
+  my_servo.write(angle);
+  Serial.print("Writing angle = ");
+  Serial.println(angle, DEC);
+}
+
+void clear_serial()
+{
+  // This function clears the serial input buffer in case of a badly feeded angle.
+  char tmp;
+  while (Serial.available() > 0)
+	tmp = Serial.read();
+}
+
+void clear_buffer(char *buffer)
+{
+  // This function clears the buffer to make sure we don't read
+  // reminiscent values from past inputs.
+  for (int i = 0; i <= max_buffer; i++)
+	buffer[i] = ' ';
 }
--- face_detection/src/face-detection.cpp
+++ face_detection/src/face-detection.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include <opencv2/core/hal/interface.h>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/objdetect.hpp>
 #include <opencv2/opencv.hpp>
 #include <opencv2/videoio.hpp>
+#include <dlib/opencv.h>
+#include <dlib/image_processing.h>
+#include <dlib/image_processing/frontal_face_detector.h>
 #include <stdexcept>
 
 int main(int argc, char *argv[])
@@ -12,6 +16,10 @@ int main(int argc, char *argv[])
   cv::Mat image, gray_image;
   int k = 0;
   std::vector<cv::Rect> faces;
+  std::vector<cv::Mat> aux_matrix(2);
+
+  for (cv::Mat& aux : aux_matrix)
+	aux = cv::Mat::zeros(250, 250, CV_8UC3);
 
   cv::namedWindow("Face detection", cv::WINDOW_AUTOSIZE);
 
@@ -31,10 +39,20 @@ int main(int argc, char *argv[])
 	// Detecting the faces on the grayscale image
 	face_cascade.detectMultiScale(gray_image, faces, 1.1, 4);
 
-	for (int i = 0; i < faces.size(); i++)
+	for (int i = 0; i < faces.size(); i++) {
 	  cv::rectangle(image, cv::Rect(faces[i].x, faces[i].y, faces[i].width, faces[i].height), cv::Scalar(255, 0, 0), 2);
+	  if (i < 2) {
+		aux_matrix[i] = image({faces[i].y, faces[i].y + faces[i].height}, {faces[i].x, faces[i].x + faces[i].width});
+	  }
+	}
+
+	if (faces.size() == 2) {
+	  image({faces[1].y, faces[1].y + faces[1].height}, {faces[1].x, faces[1].x + faces[1].width}) = aux_matrix[2];
+	  image({faces[2].y, faces[2].y + faces[2].height}, {faces[2].x, faces[2].x + faces[2].width}) = aux_matrix[1];
+	}
 
 	cv::imshow("Face detection", image);
+	// cv::imshow("Teste", aux_matrix_1);
 	k = cv::waitKey(10);
 
 	if (k == 27)
