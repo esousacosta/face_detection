3fe5b28c714992494001863358ae996a312655f6
Bacamartes <victoremanoel23@gmail.com>
2021-04-29 16:13:37 -0300

    Implemented all functions, but I'm debugging a type error

--- face_detection/src/face-detection.cpp
+++ face_detection/src/face-detection.cpp
@@ -17,6 +17,7 @@
 #include <stdexcept>
 #include <vector>
 
+
 std::vector<cv::Point2f> drawFaceLandmarks(cv::Mat &image, dlib::full_object_detection faceLandmark)
 {
   std::vector<cv::Point2f> points;
@@ -33,6 +34,7 @@ std::vector<cv::Point2f> drawFaceLandmarks(cv::Mat &image, dlib::full_object_det
   }
   return points;
 }
+
 
 // Here the Delaunay triangles are calculated for the given set of points, and the indexes for the 3 points of each
 // triangle are stored in the delaunayTri argument.
@@ -72,6 +74,64 @@ static void calculateDelaunayTriangles(cv::Rect rectangle, std::vector<cv::Point
 }
 
 
+// Apply affine transform calculated using srcTri and dstTri to src
+void applyAffineTransform(cv::Mat &warpImage, cv::Mat &src, std::vector<cv::Point2f> &srcTri, std::vector<cv::Point2f> &dstTri)
+{
+    // Given a pair of triangles, find the affine transform.
+    cv::Mat warpMat = getAffineTransform(srcTri, dstTri);
+    
+    // Apply the Affine Transform just found to the src image
+    warpAffine(src, warpImage, warpMat, warpImage.size(), cv::INTER_LINEAR, cv::BORDER_REFLECT_101);
+
+	std::cout << "Passei da função applyAffineTransform..." << std::endl;
+}
+
+
+// Warps and alpha blends triangular regions from img1 and img2 to img
+void warpTriangle(cv::Mat &img1, cv::Mat &img2, std::vector<cv::Point2f> &t1, std::vector<cv::Point2f> &t2)
+{
+	
+  cv::Rect r1 = boundingRect(t1);
+  cv::Rect r2 = boundingRect(t2);
+	
+  // Offset points by left top corner of the respective rectangles
+  std::vector<cv::Point2f> t1Rect, t2Rect;
+  std::vector<cv::Point> t2RectInt;
+  for(int i = 0; i < 3; i++)
+	{
+
+	  t1Rect.push_back( cv::Point2f( t1[i].x - r1.x, t1[i].y -  r1.y) );
+	  t2Rect.push_back( cv::Point2f( t2[i].x - r2.x, t2[i].y - r2.y) );
+	  t2RectInt.push_back( cv::Point(t2[i].x - r2.x, t2[i].y - r2.y) ); // for fillConvexPoly
+
+	}
+	
+  // Get mask by filling triangle
+  cv::Mat mask = cv::Mat::zeros(r2.height, r2.width, CV_32FC3);
+  fillConvexPoly(mask, t2RectInt, cv::Scalar(1.0, 1.0, 1.0), 16, 0);
+	
+  // Apply warpImage to small rectangular patches
+  // Here I'm taking the small rectangle delimited by r1 and copying its content (on the original image)
+  // to a small rectangle that will be "patched" onto the destination image.
+  cv::Mat img1Rect;
+  img1(r1).copyTo(img1Rect);
+	
+  cv::Mat img2Rect = cv::Mat::zeros(r2.height, r2.width, img1Rect.type());
+	
+  // Here I'm applying the affine transform to path the piece
+  // of the original image (contained in img1Rect) onto the destination
+  // rectangle (img2Rect).
+  applyAffineTransform(img2Rect, img1Rect, t1Rect, t2Rect);
+	
+  // Here I'm finally patching the second image by adding the content from the
+  // original one. This is done for each of the delaunay triangle's regions.
+  multiply(img2Rect, mask, img2Rect);
+  multiply(img2(r2), cv::Scalar(1.0,1.0,1.0) - mask, img2(r2));
+  img2(r2) = img2(r2) + img2Rect;
+	
+}
+
+
 int main(int argc, char *argv[])
 {
   cv::Mat image, gray_image;
@@ -108,8 +168,10 @@ int main(int argc, char *argv[])
   while (true) {
 	static cv::Mat image;
 	static cv::Mat copied_image;
+	static cv::Mat destination_image;
 	cap.read(image);
 	copied_image = image.clone();
+	destination_image = image.clone();
 
 	// Checking if anything was captured by the camera
 	if (!image.data)
@@ -144,14 +206,62 @@ int main(int argc, char *argv[])
 	  std::string landmarksFilename = "face_landmarks_" + std::to_string(i+1) + ".txt";
 	}
 
+
+	std::cout << "Passei do desenho das máscaras..." << std::endl;
+	
 	if (faces.size() == 2) {
 	  for (auto element: points) {
 		cv::convexHull(element, hull);
 		hulls.push_back(hull);
 	  }
 
+	  std::cout << "Passei do cálculo dos hulls..." << std::endl;
+
 	  cv::Rect rect(0, 0, copied_image.cols, copied_image.rows);
 	  calculateDelaunayTriangles(rect, hulls[0], dt);
+
+	  std::cout << "Passei do cálculo dos triangulos de delaunay..." << std::endl;
+
+	  // Now we need to apply affine transformation to delaunay triangles in order
+	  // to swap the correct points between the two faces.
+
+	  for (int i = 0; i < dt.size(); i++) {
+
+		std::vector<cv::Point2f> t1, t2;
+
+		for (int j = 0; j < 3; j++) {
+
+		  t1.push_back(hulls[0][dt[i][j]]);
+		  t2.push_back(hulls[1][dt[i][j]]);
+		  
+		}
+		// After finishing to collect the equivalente points of both contours,
+		// it's time to apply the warping to the copy of the second image.
+		warpTriangle(image, copied_image, t1, t2);
+	  }
+
+	  std::cout << "Passei do cálculo da transformada afim e do warpTriangle..." << std::endl;
+
+	  // Calculate mask
+	  std::vector<cv::Point> hull8U;
+	  for(int i = 0; i < hulls[1].size(); i++)
+		{
+		  cv::Point pt(hulls[1][i].x, hulls[1][i].y);
+		  hull8U.push_back(pt);
+		}
+
+	  cv::Mat mask = cv::Mat::zeros(copied_image.rows, copied_image.cols, copied_image.depth());
+	  fillConvexPoly(mask, &hull8U[0], hull8U.size(), cv::Scalar(255,255,255));
+
+	  // Clone seamlessly.
+	  cv::Rect r = boundingRect(hulls[1]);
+	  cv::Point center = (r.tl() + r.br()) / 2;
+	
+	  cv::Mat output;
+	  copied_image.convertTo(copied_image, CV_8UC3);
+	  cv::seamlessClone(copied_image, destination_image, mask, center, output, cv::NORMAL_CLONE);
+
+	  cv::imshow("clonned", output);
 	}
 	/* This whole commented region below was part of the original haarscascade algorithm without using dlib
 	//	
